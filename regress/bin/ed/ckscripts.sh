95058f0eb98531cbadf6d67f4cb2657f08eeaa6b
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-01-05 16:31:04 +0300

    new script to check ed tests

---
+++ regress/bin/ed/ckscripts.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -
+
+PATH="/bin:/usr/bin:/usr/local/bin/:."
+ED=$1
+OBJDIR=$2
+TESTDIR=$3
+TEST=$4
+
+[ ! -x $ED ] && { echo "$ED: cannot execute"; exit 1; }
+[ ! -e $TESTDIR/$TEST ] && { echo "$TEST: does not exist"; exit 1; }
+
+# *.red scripts must exit with non-zero status
+# *.ed scripts must exit with zero status
+ext=$(echo $TEST | cut -d'.' -f2)
+if [[ "$ext" == "ed" ]]; then
+  exit 0
+  base=`$ED - \!"echo $i" <<-EOF
+    	s/\..*
+		EOF`
+  if $OBJDIR/$base.ed; then
+	if cmp -s $TESTDIR/$base.o $TESTDIR/$base.r; then :; else
+		echo "*** Output $TESTDIR/$base.o of script $i is incorrect ***"
+		exit 1
+	fi
+  else
+    exit 1
+  fi
+else
+  if $i; then
+    exit 0
+  fi
+fi
